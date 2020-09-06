c5f2c8aea4148320e380c04e420bd238383717ad
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-09-06 13:02:05 +0200

    Create server 🗄

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
+// read and parse laptop data
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
-
 const latptopData = JSON.parse(json);
-console.log(latptopData);
-console.log(__dirname);
+
+// create web server
+const server = http.createServer((req, res) => {
+  const pathName = url.parse(req.url, true).pathname;
+  const id = url.parse(req.url, true).query.id;
+
+  if (pathName === '/products' || pathName === '/') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.end('This is products page');
+  } else if (pathName === '/laptop' && id < latptopData.length) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.end(`This is laptop page for laptop ${id}`);
+  } else {
+    res.writeHead(404, { 'Content-type': 'text/html' });
+    res.end('URL was not found on the server');
+  }
+});
+
+server.listen(1337, '127.0.0.1', () => {
+  console.log('Listening for requests now');
+});
